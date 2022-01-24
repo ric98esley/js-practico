53d3bcd81bb506233e13c8d152fef5d19be78551
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-01-23 23:14:03 -0400

    reto 3 terminado

---
+++ moda.js
@@ -0,0 +1,42 @@
+var datos = [];
+
+
+function makeObjet(list){
+    let obj = {};
+    list.map(
+        function (element) {
+            if (obj[element]) {
+                obj[element] += 1;
+            }
+            else {
+                obj[element] = 1;
+            }
+        }
+    );
+    return obj;
+}
+function makeNewArray(listCount){
+    const list = Object.entries(listCount).sort(
+        function (valorAcumulado, nuevoValor) {
+            return valorAcumulado[1] - nuevoValor[1];
+        }
+    );
+    return list;
+}
+
+
+function enviarAlArray() {
+    const input = document.getElementById("Input");
+    const value = input.value;
+//     var enviar = datos.push(value);
+    datos = value.split(",");
+}
+
+function calcular() {
+    const objecto = makeObjet(datos);
+    console.log(objecto);
+    const listArray = makeNewArray(objecto);
+    const moda = listArray[listArray.length - 1]
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = `El valor de la moda es: ${moda[0]}`;
+}
